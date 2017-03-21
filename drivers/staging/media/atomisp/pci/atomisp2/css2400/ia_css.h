ekuiter/torte-linux

commit 0f73c6088ca2fc1bc1c5f7c9a39fd8827d6f4197

staging/atomisp: Add support for the Intel IPU v2
This patch adds support for the Intel IPU v2 as found on Android and IoT
Baytrail-T and Baytrail-CR platforms (those with the IPU PCI mapped). You
will also need the firmware files from your device (Android usually puts
them into /etc) - or you can find them in the downloadable restore/upgrade
kits if you blew them away for some reason.

It may be possible to extend the driver to handle the BYT/T windows
platforms such as the ASUS T100TA. These platforms don't expose the IPU via
the PCI interface but via ACPI buried in the GPU description and with the
camera information somewhere unknown so would need a platform driver
interface adding to the codebase *IFF* the firmware works on such devices.

To get good results you also need a suitable support library such as
libxcam. The camera is intended to be driven from Android so it has a lot of
features that many desktop apps don't fully spport.

In theory all the pieces are there to build it with -DISP2401 and some
differing files to get CherryTrail/T support, but unifying the drivers
properlly is a work in progress.

The IPU driver represents the work of a lot of people within Intel over many
years. It's historical goal was portability rather than Linux upstream. Any
queries about the upstream aimed driver should be sent to me not to the
original authors.

Signed-off-by: Alan Cox <alan@linux.intel.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/media/atomisp/pci/atomisp2/Makefile
@@ -1,0 +1,380 @@
+atomisp-objs += \
+	./atomisp_drvfs.o \
+	./atomisp_file.o \
+	./css2400/sh_css_mipi.o \
+	./css2400/runtime/pipeline/src/pipeline.o \
+	./css2400/runtime/spctrl/src/spctrl.o \
+	./css2400/runtime/rmgr/src/rmgr.o \
+	./css2400/runtime/rmgr/src/rmgr_vbuf.o \
+	./css2400/runtime/isp_param/src/isp_param.o \
+	./css2400/runtime/inputfifo/src/inputfifo.o \
+	./css2400/runtime/queue/src/queue_access.o \
+	./css2400/runtime/queue/src/queue.o \
+	./css2400/runtime/frame/src/frame.o \
+	./css2400/runtime/eventq/src/eventq.o \
+	./css2400/runtime/binary/src/binary.o \
+	./css2400/runtime/timer/src/timer.o \
+	./css2400/runtime/isys/src/csi_rx_rmgr.o \
+	./css2400/runtime/isys/src/isys_stream2mmio_rmgr.o \
+	./css2400/runtime/isys/src/virtual_isys.o \
+	./css2400/runtime/isys/src/rx.o \
+	./css2400/runtime/isys/src/isys_dma_rmgr.o \
+	./css2400/runtime/isys/src/ibuf_ctrl_rmgr.o \
+	./css2400/runtime/isys/src/isys_init.o \
+	./css2400/runtime/bufq/src/bufq.o \
+	./css2400/runtime/ifmtr/src/ifmtr.o \
+	./css2400/runtime/debug/src/ia_css_debug.o \
+	./css2400/runtime/event/src/event.o \
+	./css2400/sh_css_sp.o \
+	./css2400/css_2400_system/spmem_dump.o \
+	./css2400/css_2400_system/hive_isp_css_2400_system_generated/ia_css_isp_states.o \
+	./css2400/css_2400_system/hive_isp_css_2400_system_generated/ia_css_isp_configs.o \
+	./css2400/css_2400_system/hive_isp_css_2400_system_generated/ia_css_isp_params.o \
+	./css2400/sh_css_stream_format.o \
+	./css2400/sh_css_hrt.o \
+	./css2400/sh_css_properties.o \
+	./css2400/memory_realloc.o \
+	./css2400/hive_isp_css_shared/host/tag.o \
+	./css2400/sh_css_params.o \
+	./css2400/sh_css.o \
+	./css2400/isp/kernels/hdr/ia_css_hdr.host.o \
+	./css2400/isp/kernels/uds/uds_1.0/ia_css_uds.host.o \
+	./css2400/isp/kernels/ynr/ynr_2/ia_css_ynr2.host.o \
+	./css2400/isp/kernels/ynr/ynr_1.0/ia_css_ynr.host.o \
+	./css2400/isp/kernels/conversion/conversion_1.0/ia_css_conversion.host.o \
+	./css2400/isp/kernels/fpn/fpn_1.0/ia_css_fpn.host.o \
+	./css2400/isp/kernels/tnr/tnr_1.0/ia_css_tnr.host.o \
+	./css2400/isp/kernels/vf/vf_1.0/ia_css_vf.host.o \
+	./css2400/isp/kernels/raw/raw_1.0/ia_css_raw.host.o \
+	./css2400/isp/kernels/ref/ref_1.0/ia_css_ref.host.o \
+	./css2400/isp/kernels/qplane/qplane_2/ia_css_qplane.host.o \
+	./css2400/isp/kernels/norm/norm_1.0/ia_css_norm.host.o \
+	./css2400/isp/kernels/output/output_1.0/ia_css_output.host.o \
+	./css2400/isp/kernels/raw_aa_binning/raw_aa_binning_1.0/ia_css_raa.host.o \
+	./css2400/isp/kernels/macc/macc1_5/ia_css_macc1_5_table.host.o \
+	./css2400/isp/kernels/macc/macc1_5/ia_css_macc1_5.host.o \
+	./css2400/isp/kernels/macc/macc_1.0/ia_css_macc.host.o \
+	./css2400/isp/kernels/macc/macc_1.0/ia_css_macc_table.host.o \
+	./css2400/isp/kernels/csc/csc_1.0/ia_css_csc.host.o \
+	./css2400/isp/kernels/iefd2_6/ia_css_iefd2_6_default.host.o \
+	./css2400/isp/kernels/iefd2_6/ia_css_iefd2_6.host.o \
+	./css2400/isp/kernels/bnr/bnr_1.0/ia_css_bnr.host.o \
+	./css2400/isp/kernels/bnr/bnr2_2/ia_css_bnr2_2.host.o \
+	./css2400/isp/kernels/dpc2/ia_css_dpc2.host.o \
+	./css2400/isp/kernels/dpc2/ia_css_dpc2_default.host.o \
+	./css2400/isp/kernels/fc/fc_1.0/ia_css_formats.host.o \
+	./css2400/isp/kernels/ctc/ctc_1.0/ia_css_ctc.host.o \
+	./css2400/isp/kernels/ctc/ctc_1.0/ia_css_ctc_table.host.o \
+	./css2400/isp/kernels/ctc/ctc2/ia_css_ctc2.host.o \
+	./css2400/isp/kernels/ctc/ctc1_5/ia_css_ctc1_5.host.o \
+	./css2400/isp/kernels/fixedbds/fixedbds_1.0/ia_css_fixedbds.host.o \
+	./css2400/isp/kernels/bh/bh_2/ia_css_bh.host.o \
+	./css2400/isp/kernels/bnlm/ia_css_bnlm_default.host.o \
+	./css2400/isp/kernels/bnlm/ia_css_bnlm.host.o \
+	./css2400/isp/kernels/tdf/tdf_1.0/ia_css_tdf.host.o \
+	./css2400/isp/kernels/tdf/tdf_1.0/ia_css_tdf_default.host.o \
+	./css2400/isp/kernels/dvs/dvs_1.0/ia_css_dvs.host.o \
+	./css2400/isp/kernels/anr/anr_1.0/ia_css_anr.host.o \
+	./css2400/isp/kernels/anr/anr_2/ia_css_anr2_table.host.o \
+	./css2400/isp/kernels/anr/anr_2/ia_css_anr2.host.o \
+	./css2400/isp/kernels/dp/dp_1.0/ia_css_dp.host.o \
+	./css2400/isp/kernels/sdis/sdis_1.0/ia_css_sdis.host.o \
+	./css2400/isp/kernels/sdis/sdis_2/ia_css_sdis2.host.o \
+	./css2400/isp/kernels/cnr/cnr_2/ia_css_cnr2.host.o \
+	./css2400/isp/kernels/cnr/cnr_1.0/ia_css_cnr.host.o \
+	./css2400/isp/kernels/satm/ia_css_satm.host.o \
+	./css2400/isp/kernels/xnr/xnr_1.0/ia_css_xnr.host.o \
+	./css2400/isp/kernels/xnr/xnr_1.0/ia_css_xnr_table.host.o \
+	./css2400/isp/kernels/xnr/xnr3_0_5/ia_css_xnr3_0_5.host.o \
+	./css2400/isp/kernels/xnr/xnr_3.0/ia_css_xnr3.host.o \
+	./css2400/isp/kernels/xnr/xnr3_0_11/ia_css_xnr3_0_11.host.o \
+	./css2400/isp/kernels/de/de_1.0/ia_css_de.host.o \
+	./css2400/isp/kernels/de/de_2/ia_css_de2.host.o \
+	./css2400/isp/kernels/gc/gc_2/ia_css_gc2.host.o \
+	./css2400/isp/kernels/gc/gc_2/ia_css_gc2_table.host.o \
+	./css2400/isp/kernels/gc/gc_1.0/ia_css_gc.host.o \
+	./css2400/isp/kernels/gc/gc_1.0/ia_css_gc_table.host.o \
+	./css2400/isp/kernels/crop/crop_1.0/ia_css_crop.host.o \
+	./css2400/isp/kernels/io_ls/bayer_io_ls/ia_css_bayer_io.host.o \
+	./css2400/isp/kernels/aa/aa_2/ia_css_aa2.host.o \
+	./css2400/isp/kernels/pdaf/ia_css_pdaf.host.o \
+	./css2400/isp/kernels/copy_output/copy_output_1.0/ia_css_copy_output.host.o \
+	./css2400/isp/kernels/ob/ob_1.0/ia_css_ob.host.o \
+	./css2400/isp/kernels/ob/ob2/ia_css_ob2.host.o \
+	./css2400/isp/kernels/iterator/iterator_1.0/ia_css_iterator.host.o \
+	./css2400/isp/kernels/wb/wb_1.0/ia_css_wb.host.o \
+	./css2400/isp/kernels/eed1_8/ia_css_eed1_8_default.host.o \
+	./css2400/isp/kernels/eed1_8/ia_css_eed1_8.host.o \
+	./css2400/isp/kernels/sc/sc_1.0/ia_css_sc.host.o \
+	./css2400/isp/kernels/ipu2_io_ls/bayer_io_ls/ia_css_bayer_io.host.o \
+	./css2400/isp/kernels/ipu2_io_ls/yuv444_io_ls/ia_css_yuv444_io.host.o \
+	./css2400/isp/kernels/s3a/s3a_1.0/ia_css_s3a.host.o \
+	./css2400/sh_css_lace_stat.o \
+	./css2400/sh_css_pipe.o \
+	./css2400/ia_css_device_access.o \
+	./css2400/sh_css_host_data.o \
+	./css2400/sh_css_mmu.o \
+	./css2400/sh_css_metadata.o \
+	./css2400/base/refcount/src/refcount.o \
+	./css2400/base/circbuf/src/circbuf.o \
+	./css2400/sh_css_irq.o \
+	./css2400/camera/pipe/src/pipe_binarydesc.o \
+	./css2400/camera/pipe/src/pipe_util.o \
+	./css2400/camera/pipe/src/pipe_stagedesc.o \
+	./css2400/camera/util/src/util.o \
+	./css2400/sh_css_metrics.o \
+	./css2400/sh_css_version.o \
+	./css2400/ia_css_memory_access.o \
+	./css2400/sh_css_param_shading.o \
+	./css2400/sh_css_morph.o \
+	./css2400/sh_css_firmware.o \
+	./css2400/hive_isp_css_common/host/isp.o \
+	./css2400/hive_isp_css_common/host/gdc.o \
+	./css2400/hive_isp_css_common/host/sp.o \
+	./css2400/hive_isp_css_common/host/vmem.o \
+	./css2400/hive_isp_css_common/host/dma.o \
+	./css2400/hive_isp_css_common/host/input_formatter.o \
+	./css2400/hive_isp_css_common/host/debug.o \
+	./css2400/hive_isp_css_common/host/hmem.o \
+	./css2400/hive_isp_css_common/host/gp_device.o \
+	./css2400/hive_isp_css_common/host/fifo_monitor.o \
+	./css2400/hive_isp_css_common/host/gp_timer.o \
+	./css2400/hive_isp_css_common/host/irq.o \
+	./css2400/hive_isp_css_common/host/input_system.o \
+	./css2400/hive_isp_css_common/host/timed_ctrl.o \
+	./css2400/hive_isp_css_common/host/mmu.o \
+	./css2400/hive_isp_css_common/host/event_fifo.o \
+	./css2400/sh_css_param_dvs.o \
+	./css2400/sh_css_shading.o \
+	./css2400/sh_css_stream.o \
+	./mmu/sh_mmu_mrfld.o \
+	./mmu/isp_mmu.o \
+	./atomisp_acc.o \
+	./atomisp_compat_css20.o \
+	./atomisp_fops.o \
+	./atomisp_subdev.o \
+	./atomisp_ioctl.o \
+	./atomisp_compat_ioctl32.o \
+	./atomisp_csi2.o \
+	./atomisp_cmd.o \
+	./atomisp_tpg.o \
+	./hmm/hmm_vm.o \
+	./hmm/hmm.o \
+	./hmm/hmm_bo.o \
+	./hmm/hmm_reserved_pool.o \
+	./hmm/hmm_dynamic_pool.o \
+	./hrt/hive_isp_css_mm_hrt.o \
+	./atomisp_v4l2.o
+	
+extra= \
+	./hrt/hive_isp_css_mm_hrt.o \
+	./hrt/memory_access.o \
+	./hrt/device_access.o \
+	./hmm/hmm_dynamic_pool.o \
+	./hmm/hmm_vm.o \
+	./hmm/hmm_reserved_pool.o \
+	./hmm/hmm_bo_dev.o \
+	./hmm/hmm.o \
+	./hmm/hmm_bo.o
+
+# These will be needed when clean merge CHT support nicely into the driver
+# Keep them here handy for when we get to that point
+#
+
+obj-cht= \
+	./css2400/css_2401_system/spmem_dump.o \
+	./css2400/css_2401_system/hive_isp_css_2401_system_generated/ia_css_isp_states.o \
+	./css2400/css_2401_system/hive_isp_css_2401_system_generated/ia_css_isp_configs.o \
+	./css2400/css_2401_system/hive_isp_css_2401_system_generated/ia_css_isp_params.o \
+	./css2400/css_2401_csi2p_system/spmem_dump.o \
+	./css2400/css_2401_csi2p_system/host/isys_stream2mmio.o \
+	./css2400/css_2401_csi2p_system/host/ibuf_ctrl.o \
+	./css2400/css_2401_csi2p_system/host/isys_irq.o \
+	./css2400/css_2401_csi2p_system/host/isys_dma.o \
+	./css2400/css_2401_csi2p_system/host/csi_rx.o \
+	./css2400/css_2401_csi2p_system/hive_isp_css_2401_system_csi2p_generated/ia_css_isp_states.o \
+	./css2400/css_2401_csi2p_system/hive_isp_css_2401_system_csi2p_generated/ia_css_isp_configs.o \
+	./css2400/css_2401_csi2p_system/hive_isp_css_2401_system_csi2p_generated/ia_css_isp_params.o \
+
+#	-I$(atomisp)/css2400/css_2401_csi2p_system/ \
+#	-I$(atomisp)/css2400/css_2401_csi2p_system/hive_isp_css_2401_system_csi2p_generated/ \
+#	-I$(atomisp)/css2400/css_2401_csi2p_system/host/ \
+#	-I$(atomisp)/css2400/css_2401_csi2p_system/hrt/ \
+#	-I$(atomisp)/css2400/css_2401_system/hive_isp_css_2401_system_generated/ \
+#	-I$(atomisp)/css2400/css_2401_system/hrt/ \
+
+
+
+obj-$(CONFIG_VIDEO_ATOMISP) += atomisp.o
+
+atomisp = $(srctree)/drivers/staging/media/atomisp/pci/atomisp2
+
+INCLUDES += \
+	-I$(atomisp)/ \
+	-I$(atomisp)/css2400/ \
+	-I$(atomisp)/hrt/ \
+	-I$(atomisp)/include/ \
+	-I$(atomisp)/include/hmm/ \
+	-I$(atomisp)/include/mmu/ \
+	-I$(atomisp)/css2400/base/circbuf/interface/ \
+	-I$(atomisp)/css2400/base/refcount/interface/ \
+	-I$(atomisp)/css2400/camera/pipe/interface/ \
+	-I$(atomisp)/css2400/camera/util/interface/ \
+	-I$(atomisp)/css2400/css_2400_system/ \
+	-I$(atomisp)/css2400/css_2400_system/hive_isp_css_2400_system_generated/ \
+	-I$(atomisp)/css2400/css_2400_system/hrt/ \
+	-I$(atomisp)/css2400/hive_isp_css_common/ \
+	-I$(atomisp)/css2400/hive_isp_css_common/host/ \
+	-I$(atomisp)/css2400/hive_isp_css_include/ \
+	-I$(atomisp)/css2400/hive_isp_css_include/device_access/ \
+	-I$(atomisp)/css2400/hive_isp_css_include/host/ \
+	-I$(atomisp)/css2400/hive_isp_css_include/memory_access/ \
+	-I$(atomisp)/css2400/hive_isp_css_shared/ \
+	-I$(atomisp)/css2400/hive_isp_css_shared/host/ \
+	-I$(atomisp)/css2400/isp/kernels/ \
+	-I$(atomisp)/css2400/isp/kernels/aa/aa_2/ \
+	-I$(atomisp)/css2400/isp/kernels/anr/anr_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/anr/anr_2/ \
+	-I$(atomisp)/css2400/isp/kernels/bayer_ls/bayer_ls_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/bh/bh_2/ \
+	-I$(atomisp)/css2400/isp/kernels/bnlm/ \
+	-I$(atomisp)/css2400/isp/kernels/bnr/ \
+	-I$(atomisp)/css2400/isp/kernels/bnr/bnr_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/bnr/bnr2_2/ \
+	-I$(atomisp)/css2400/isp/kernels/cnr/ \
+	-I$(atomisp)/css2400/isp/kernels/cnr/cnr_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/cnr/cnr_2/ \
+	-I$(atomisp)/css2400/isp/kernels/conversion/ \
+	-I$(atomisp)/css2400/isp/kernels/conversion/conversion_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/copy_output/ \
+	-I$(atomisp)/css2400/isp/kernels/copy_output/copy_output_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/crop/ \
+	-I$(atomisp)/css2400/isp/kernels/crop/crop_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/csc/ \
+	-I$(atomisp)/css2400/isp/kernels/csc/csc_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/ctc/ \
+	-I$(atomisp)/css2400/isp/kernels/ctc/ctc_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/ctc/ctc1_5/ \
+	-I$(atomisp)/css2400/isp/kernels/ctc/ctc2/ \
+	-I$(atomisp)/css2400/isp/kernels/de/ \
+	-I$(atomisp)/css2400/isp/kernels/de/de_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/de/de_2/ \
+	-I$(atomisp)/css2400/isp/kernels/dpc2/ \
+	-I$(atomisp)/css2400/isp/kernels/dp/ \
+	-I$(atomisp)/css2400/isp/kernels/dp/dp_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/dvs/ \
+	-I$(atomisp)/css2400/isp/kernels/dvs/dvs_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/eed1_8/ \
+	-I$(atomisp)/css2400/isp/kernels/fc/ \
+	-I$(atomisp)/css2400/isp/kernels/fc/fc_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/fixedbds/ \
+	-I$(atomisp)/css2400/isp/kernels/fixedbds/fixedbds_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/fpn/ \
+	-I$(atomisp)/css2400/isp/kernels/fpn/fpn_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/gc/ \
+	-I$(atomisp)/css2400/isp/kernels/gc/gc_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/gc/gc_2/ \
+	-I$(atomisp)/css2400/isp/kernels/hdr/ \
+	-I$(atomisp)/css2400/isp/kernels/iefd2_6/ \
+	-I$(atomisp)/css2400/isp/kernels/io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/io_ls/bayer_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/io_ls/common/ \
+	-I$(atomisp)/css2400/isp/kernels/io_ls/plane_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/io_ls/yuv420_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/io_ls/yuv444_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/ipu2_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/ipu2_io_ls/bayer_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/ipu2_io_ls/common/ \
+	-I$(atomisp)/css2400/isp/kernels/ipu2_io_ls/plane_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/ipu2_io_ls/yuv420_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/ipu2_io_ls/yuv444_io_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/iterator/ \
+	-I$(atomisp)/css2400/isp/kernels/iterator/iterator_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/macc/ \
+	-I$(atomisp)/css2400/isp/kernels/macc/macc_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/macc/macc1_5/ \
+	-I$(atomisp)/css2400/isp/kernels/norm/ \
+	-I$(atomisp)/css2400/isp/kernels/norm/norm_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/ob/ \
+	-I$(atomisp)/css2400/isp/kernels/ob/ob_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/ob/ob2/ \
+	-I$(atomisp)/css2400/isp/kernels/output/ \
+	-I$(atomisp)/css2400/isp/kernels/output/output_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/pdaf/ \
+	-I$(atomisp)/css2400/isp/kernels/qplane/ \
+	-I$(atomisp)/css2400/isp/kernels/qplane/qplane_2/ \
+	-I$(atomisp)/css2400/isp/kernels/raw_aa_binning/ \
+	-I$(atomisp)/css2400/isp/kernels/raw_aa_binning/raw_aa_binning_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/raw/ \
+	-I$(atomisp)/css2400/isp/kernels/raw/raw_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/ref/ \
+	-I$(atomisp)/css2400/isp/kernels/ref/ref_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/s3a/ \
+	-I$(atomisp)/css2400/isp/kernels/s3a/s3a_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/s3a_stat_ls/ \
+	-I$(atomisp)/css2400/isp/kernels/satm/ \
+	-I$(atomisp)/css2400/isp/kernels/scale/ \
+	-I$(atomisp)/css2400/isp/kernels/scale/scale_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/sc/ \
+	-I$(atomisp)/css2400/isp/kernels/sc/sc_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/sdis/ \
+	-I$(atomisp)/css2400/isp/kernels/sdis/common/ \
+	-I$(atomisp)/css2400/isp/kernels/sdis/sdis_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/sdis/sdis_2/ \
+	-I$(atomisp)/css2400/isp/kernels/tdf/ \
+	-I$(atomisp)/css2400/isp/kernels/tdf/tdf_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/tnr/ \
+	-I$(atomisp)/css2400/isp/kernels/tnr/tnr_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/tnr/tnr3/ \
+	-I$(atomisp)/css2400/isp/kernels/uds/ \
+	-I$(atomisp)/css2400/isp/kernels/uds/uds_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/vf/ \
+	-I$(atomisp)/css2400/isp/kernels/vf/vf_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/wb/ \
+	-I$(atomisp)/css2400/isp/kernels/wb/wb_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/xnr/ \
+	-I$(atomisp)/css2400/isp/kernels/xnr/xnr_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/xnr/xnr_3.0/ \
+	-I$(atomisp)/css2400/isp/kernels/xnr/xnr3_0_11 \
+	-I$(atomisp)/css2400/isp/kernels/xnr/xnr3_0_5/ \
+	-I$(atomisp)/css2400/isp/kernels/ynr/ \
+	-I$(atomisp)/css2400/isp/kernels/ynr/ynr_1.0/ \
+	-I$(atomisp)/css2400/isp/kernels/ynr/ynr_2/ \
+	-I$(atomisp)/css2400/isp/kernels/yuv_ls \
+	-I$(atomisp)/css2400/isp/kernels/yuv_ls/yuv_ls_1.0/ \
+	-I$(atomisp)/css2400/isp/modes/interface/ \
+	-I$(atomisp)/css2400/runtime/binary/interface/ \
+	-I$(atomisp)/css2400/runtime/bufq/interface/ \
+	-I$(atomisp)/css2400/runtime/debug/interface/ \
+	-I$(atomisp)/css2400/runtime/event/interface/ \
+	-I$(atomisp)/css2400/runtime/eventq/interface/ \
+	-I$(atomisp)/css2400/runtime/frame/interface/ \
+	-I$(atomisp)/css2400/runtime/ifmtr/interface/ \
+	-I$(atomisp)/css2400/runtime/inputfifo/interface/ \
+	-I$(atomisp)/css2400/runtime/isp_param/interface/ \
+	-I$(atomisp)/css2400/runtime/isys/interface/ \
+	-I$(atomisp)/css2400/runtime/isys/src/ \
+	-I$(atomisp)/css2400/runtime/pipeline/interface/ \
+	-I$(atomisp)/css2400/runtime/queue/interface/ \
+	-I$(atomisp)/css2400/runtime/queue/src/ \
+	-I$(atomisp)/css2400/runtime/rmgr/interface/ \
+	-I$(atomisp)/css2400/runtime/spctrl/interface/ \
+	-I$(atomisp)/css2400/runtime/tagger/interface/
+
+ifeq ($(CONFIG_ION),y)
+INCLUDES += -I$(srctree)/drivers/staging/android/ion
+endif
+
+DEFINES := -DHRT_HW -DHRT_ISP_CSS_CUSTOM_HOST -DHRT_USE_VIR_ADDRS -DHRT_KERNEL -D__HOST__
+DEFINES += -DUSE_DYNAMIC_BIN
+DEFINES += -DISP_POWER_GATING
+DEFINES += -DUSE_INTERRUPTS
+#DEFINES += -DUSE_SSSE3
+DEFINES += -DPUNIT_CAMERA_BUSY
+DEFINES += -DUSE_KMEM_CACHE
+
+DEFINES += -DATOMISP_POSTFIX=\"css2400b0_v21\" -DISP2400B0
+DEFINES += -DSYSTEM_hive_isp_css_2400_system -DISP2400
+
+ccflags-y += $(INCLUDES) $(DEFINES) -fno-common -Werror
+

drivers/staging/media/atomisp/pci/atomisp2/css2400/ia_css.h
@@ -1,0 +1,58 @@
+/* Release Version: irci_stable_candrpv_0415_20150521_0458 */
+/*
+ * Support for Intel Camera Imaging ISP subsystem.
+ * Copyright (c) 2015, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef _IA_CSS_H_
+#define _IA_CSS_H_
+
+/** @file
+ * This file is the starting point of the CSS-API. It includes all CSS-API
+ * header files.
+ */
+
+#include "ia_css_3a.h"
+#include "ia_css_acc_types.h"
+#include "ia_css_buffer.h"
+#include "ia_css_control.h"
+#include "ia_css_device_access.h"
+#include "ia_css_dvs.h"
+#include "ia_css_env.h"
+#include "ia_css_err.h"
+#include "ia_css_event_public.h"
+#include "ia_css_firmware.h"
+#include "ia_css_frame_public.h"
+#include "ia_css_input_port.h"
+#include "ia_css_irq.h"
+#include "ia_css_metadata.h"
+#include "ia_css_mipi.h"
+#include "ia_css_pipe_public.h"
+#include "ia_css_prbs.h"
+#include "ia_css_properties.h"
+#include "ia_css_stream_format.h"
+#include "ia_css_stream_public.h"
+#include "ia_css_tpg.h"
+#include "ia_css_lace_stat.h"
+#include "ia_css_version.h"
+#include "ia_css_mmu.h"
+#include "ia_css_morph.h"
+#include "ia_css_shading.h"
+#include "ia_css_timer.h"
+
+/*
+   Please do not add code to this file. Public functionality is to be
+   exposed in a function/data type specific header file.
+   Please add to the appropriate header file or create a new one.
+ */
+
+#endif /* _IA_CSS_H_ */

drivers/staging/media/atomisp/pci/atomisp2/css2400/ia_css_buffer.h
@@ -1,0 +1,85 @@
+/*
+ * Support for Intel Camera Imaging ISP subsystem.
+ * Copyright (c) 2015, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+
+#ifndef __IA_CSS_BUFFER_H
+#define __IA_CSS_BUFFER_H
+
+/** @file
+ * This file contains datastructures and types for buffers used in CSS
+ */
+
+#include <type_support.h>
+#include "ia_css_types.h"
+#include "ia_css_timer.h"
+
+/** Enumeration of buffer types. Buffers can be queued and de-queued
+ *  to hand them over between IA and ISP.
+ */
+enum ia_css_buffer_type {
+	IA_CSS_BUFFER_TYPE_INVALID = -1,
+	IA_CSS_BUFFER_TYPE_3A_STATISTICS = 0,
+	IA_CSS_BUFFER_TYPE_DIS_STATISTICS,
+	IA_CSS_BUFFER_TYPE_LACE_STATISTICS,
+	IA_CSS_BUFFER_TYPE_INPUT_FRAME,
+	IA_CSS_BUFFER_TYPE_OUTPUT_FRAME,
+	IA_CSS_BUFFER_TYPE_SEC_OUTPUT_FRAME,
+	IA_CSS_BUFFER_TYPE_VF_OUTPUT_FRAME,
+	IA_CSS_BUFFER_TYPE_SEC_VF_OUTPUT_FRAME,
+	IA_CSS_BUFFER_TYPE_RAW_OUTPUT_FRAME,
+	IA_CSS_BUFFER_TYPE_CUSTOM_INPUT,
+	IA_CSS_BUFFER_TYPE_CUSTOM_OUTPUT,
+	IA_CSS_BUFFER_TYPE_METADATA,
+	IA_CSS_BUFFER_TYPE_PARAMETER_SET,
+	IA_CSS_BUFFER_TYPE_PER_FRAME_PARAMETER_SET,
+	IA_CSS_NUM_DYNAMIC_BUFFER_TYPE,
+	IA_CSS_NUM_BUFFER_TYPE
+};
+
+/* Driver API is not SP/ISP visible, 64 bit types not supported on hivecc */
+#if !defined(__SP) && !defined(__ISP)
+/** Buffer structure. This is a container structure that enables content
+ *  independent buffer queues and access functions.
+ */
+struct ia_css_buffer {
+	enum ia_css_buffer_type type; /**< Buffer type. */
+	unsigned int exp_id;
+	/**< exposure id for this buffer; 0 = not available
+	     see ia_css_event_public.h for more detail. */
+	union {
+		struct ia_css_isp_3a_statistics  *stats_3a;    /**< 3A statistics & optionally RGBY statistics. */
+		struct ia_css_isp_dvs_statistics *stats_dvs;   /**< DVS statistics. */
+		struct ia_css_isp_skc_dvs_statistics *stats_skc_dvs;  /**< SKC DVS statistics. */
+		struct ia_css_isp_lace_statistics *stats_lace; /**< LACE statistics. */
+		struct ia_css_frame              *frame;       /**< Frame buffer. */
+		struct ia_css_acc_param          *custom_data; /**< Custom buffer. */
+		struct ia_css_metadata           *metadata;    /**< Sensor metadata. */
+	} data; /**< Buffer data pointer. */
+	uint64_t driver_cookie; /**< cookie for the driver */
+	struct ia_css_time_meas timing_data; /**< timing data (readings from the timer) */
+	struct ia_css_clock_tick isys_eof_clock_tick; /**< ISYS's end of frame timer tick*/
+};
+
+/** @brief Dequeue param buffers from sp2host_queue
+ *
+ * @return                                       None
+ *
+ * This function must be called at every driver interrupt handler to prevent
+ * overflow of sp2host_queue.
+ */
+void
+ia_css_dequeue_param_buffers(void);
+
+#endif /* !__SP && !__ISP */
+
+#endif /* __IA_CSS_BUFFER_H */

commit 79224099120338ccc337d5cedb8e6d0160994d37

staging/atomisp: remove sh_css_lace_stat code
I ran into a build warning on my randconfig build box:

drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_params.c: In function 'ia_css_lace_statistics_free':
drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_params.c:2845:64: error: parameter 'me' set but not used [-Werror=unused-but-set-parameter]

It turns out that not only the parameter is unused but the entire function has no
caller. Let's just remove it.

Fixes: 0f73c6088ca2 ("staging/atomisp: Add support for the Intel IPU v2")
Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Signed-off-by: Alan Cox <alan@linux.intel.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/media/atomisp/pci/atomisp2/Makefile
@@ -106,7 +106,6 @@ atomisp-objs += \
 	css2400/isp/kernels/ipu2_io_ls/bayer_io_ls/ia_css_bayer_io.host.o \
 	css2400/isp/kernels/ipu2_io_ls/yuv444_io_ls/ia_css_yuv444_io.host.o \
 	css2400/isp/kernels/s3a/s3a_1.0/ia_css_s3a.host.o \
-	css2400/sh_css_lace_stat.o \
 	css2400/sh_css_pipe.o \
 	css2400/ia_css_device_access.o \
 	css2400/sh_css_host_data.o \

drivers/staging/media/atomisp/pci/atomisp2/css2400/ia_css.h
@@ -42,7 +42,6 @@
 #include "ia_css_stream_format.h"
 #include "ia_css_stream_public.h"
 #include "ia_css_tpg.h"
-#include "ia_css_lace_stat.h"
 #include "ia_css_version.h"
 #include "ia_css_mmu.h"
 #include "ia_css_morph.h"

drivers/staging/media/atomisp/pci/atomisp2/css2400/ia_css_buffer.h
@@ -60,7 +60,6 @@ struct ia_css_buffer {
 		struct ia_css_isp_3a_statistics  *stats_3a;    /**< 3A statistics & optionally RGBY statistics. */
 		struct ia_css_isp_dvs_statistics *stats_dvs;   /**< DVS statistics. */
 		struct ia_css_isp_skc_dvs_statistics *stats_skc_dvs;  /**< SKC DVS statistics. */
-		struct ia_css_isp_lace_statistics *stats_lace; /**< LACE statistics. */
 		struct ia_css_frame              *frame;       /**< Frame buffer. */
 		struct ia_css_acc_param          *custom_data; /**< Custom buffer. */
 		struct ia_css_metadata           *metadata;    /**< Sensor metadata. */

drivers/staging/media/atomisp/pci/atomisp2/css2400/ia_css_lace_stat.h
@@ -1,37 +1,0 @@
-/*
- * Support for Intel Camera Imaging ISP subsystem.
- * Copyright (c) 2015, Intel Corporation.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- */
-
-#ifndef __IA_CSS_LACE_STAT_H
-#define __IA_CSS_LACE_STAT_H
-
-/** @file
- * This file contains types used for LACE statistics
- */
-
-struct ia_css_isp_lace_statistics;
-
-/** @brief Allocate mem for the LACE statistics on the ISP
- * @return	Pointer to the allocated LACE statistics
- *         buffer on the ISP
-*/
-struct ia_css_isp_lace_statistics *ia_css_lace_statistics_allocate(void);
-
-/** @brief Free the ACC LACE statistics memory on the isp
- * @param[in]	me Pointer to the LACE statistics buffer on the
- *       ISP.
- * @return		None
-*/
-void ia_css_lace_statistics_free(struct ia_css_isp_lace_statistics *me);
-
-#endif /*  __IA_CSS_LACE_STAT_H */

drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_internal.h
@@ -44,7 +44,6 @@
 #include "ia_css_frame_comm.h"
 #include "ia_css_3a.h"
 #include "ia_css_dvs.h"
-#include "ia_css_lace_stat.h"
 #include "ia_css_metadata.h"
 #include "runtime/bufq/interface/ia_css_bufq.h"
 #include "ia_css_timer.h"

drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_lace_stat.c
@@ -1,16 +1,0 @@
-/*
- * Support for Intel Camera Imaging ISP subsystem.
- * Copyright (c) 2015, Intel Corporation.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- */
-
-/* This file will contain the code to implement the functions declared in ia_css_lace_stat.h
-   and associated helper functions */

drivers/staging/media/atomisp/pci/atomisp2/css2400/sh_css_params.c
@@ -1225,10 +1225,6 @@ struct ia_css_isp_skc_dvs_statistics {
 	ia_css_ptr p_data;
 };
 
-struct ia_css_isp_lace_statistics {
-	ia_css_ptr p_data;
-};
-
 static enum ia_css_err
 ref_sh_css_ddr_address_map(
 		struct sh_css_ddr_address_map *map,
@@ -2835,17 +2831,6 @@ ia_css_isp_3a_statistics_free(struct ia_css_isp_3a_statistics *me)
 }
 
 struct ia_css_isp_skc_dvs_statistics *ia_css_skc_dvs_statistics_allocate(void)
-{
-	return NULL;
-}
-
-void
-ia_css_lace_statistics_free(struct ia_css_isp_lace_statistics *me)
-{
-	me = NULL;
-}
-
-struct ia_css_isp_lace_statistics *ia_css_lace_statistics_allocate(void)
 {
 	return NULL;
 }
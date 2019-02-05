ekuiter/torte-linux

commit f55670097b0fed263fea58b1998c06c13a83e454

MIPS: mm: Un-inline get_new_mmu_context
In preparation for adding MMID support to get_new_mmu_context() which
will increase the size of the function somewhat, move it from
asm/mmu_context.h into a C file.

Signed-off-by: Paul Burton <paul.burton@mips.com>
Cc: linux-mips@vger.kernel.org

arch/mips/mm/context.c
@@ -1,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/mmu_context.h>
+
+void get_new_mmu_context(struct mm_struct *mm)
+{
+	unsigned int cpu;
+	u64 asid;
+
+	cpu = smp_processor_id();
+	asid = asid_cache(cpu);
+
+	if (!((asid += cpu_asid_inc()) & cpu_asid_mask(&cpu_data[cpu]))) {
+		if (cpu_has_vtag_icache)
+			flush_icache_all();
+		local_flush_tlb_all();	/* start new asid cycle */
+	}
+
+	cpu_context(cpu, mm) = asid_cache(cpu) = asid;
+}

arch/mips/include/asm/mmu_context.h
@@ -97,25 +97,7 @@ static inline void enter_lazy_tlb(struct mm_struct *mm, struct task_struct *tsk)
 {
 }
 
-
-/* Normal, classic MIPS get_new_mmu_context */
-static inline void
-get_new_mmu_context(struct mm_struct *mm)
-{
-	unsigned int cpu;
-	u64 asid;
-
-	cpu = smp_processor_id();
-	asid = asid_cache(cpu);
-
-	if (!((asid += cpu_asid_inc()) & cpu_asid_mask(&cpu_data[cpu]))) {
-		if (cpu_has_vtag_icache)
-			flush_icache_all();
-		local_flush_tlb_all();	/* start new asid cycle */
-	}
-
-	cpu_context(cpu, mm) = asid_cache(cpu) = asid;
-}
+extern void get_new_mmu_context(struct mm_struct *mm);
 
 /*
  * Initialize the context related info for a new mm_struct

arch/mips/mm/Makefile
@@ -4,6 +4,7 @@
 #
 
 obj-y				+= cache.o
+obj-y				+= context.o
 obj-y				+= extable.o
 obj-y				+= fault.o
 obj-y				+= gup.o

commit df803538c3e290469b45f99ed46055df1c2f3750

MIPS: Export mm switching functions used by KVM
KVM makes use of check_switch_mmu_context(), check_mmu_context() &
get_new_mmu_context() which are no longer static inline functions in a
header. As such they need to be exported for KVM to successfully build
as a module, which was previously overlooked. Add the missing exports.

Signed-off-by: Paul Burton <paul.burton@mips.com>
Fixes: f55670097b0f ("MIPS: mm: Un-inline get_new_mmu_context")
Fixes: b41e2e9142ce ("MIPS: mm: Unify ASID version checks")

arch/mips/mm/context.c
@@ -42,6 +42,7 @@ void get_new_mmu_context(struct mm_struct *mm)
 	set_cpu_context(cpu, mm, asid);
 	asid_cache(cpu) = asid;
 }
+EXPORT_SYMBOL_GPL(get_new_mmu_context);
 
 void check_mmu_context(struct mm_struct *mm)
 {
@@ -58,6 +59,7 @@ void check_mmu_context(struct mm_struct *mm)
 	if (!asid_versions_eq(cpu, cpu_context(cpu, mm), asid_cache(cpu)))
 		get_new_mmu_context(mm);
 }
+EXPORT_SYMBOL_GPL(check_mmu_context);
 
 static void flush_context(void)
 {
@@ -260,6 +262,7 @@ void check_switch_mmu_context(struct mm_struct *mm)
 setup_pgd:
 	TLBMISS_HANDLER_SETUP_PGD(mm->pgd);
 }
+EXPORT_SYMBOL_GPL(check_switch_mmu_context);
 
 static int mmid_init(void)
 {
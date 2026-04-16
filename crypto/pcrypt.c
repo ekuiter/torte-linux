ekuiter/torte-linux

commit 2c6e68fbc52383306c4d5a02056c44a96abcf42a

crypto: pcrypt - call the complete function on error
This fixes three forgotten calls to the complete function
in the error case.

Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

crypto/pcrypt.c
@@ -114,7 +114,7 @@ static void pcrypt_aead_enc(struct padata_priv *padata)
 
 	padata->info = crypto_aead_encrypt(req);
 
-	if (padata->info)
+	if (padata->info == -EINPROGRESS)
 		return;
 
 	padata_do_serial(padata);
@@ -158,7 +158,7 @@ static void pcrypt_aead_dec(struct padata_priv *padata)
 
 	padata->info = crypto_aead_decrypt(req);
 
-	if (padata->info)
+	if (padata->info == -EINPROGRESS)
 		return;
 
 	padata_do_serial(padata);
@@ -202,7 +202,7 @@ static void pcrypt_aead_givenc(struct padata_priv *padata)
 
 	padata->info = crypto_aead_givencrypt(req);
 
-	if (padata->info)
+	if (padata->info == -EINPROGRESS)
 		return;
 
 	padata_do_serial(padata);

commit 2ac29c868e3d0a52e9c7c002ab1475d911ec62bd

crypto: pcrypt - Fix handling of MAY_BACKLOG requests
MAY_BACKLOG requests can return EBUSY.  Handle them by checking
for that value and filtering out EINPROGRESS notifications.

Reported-by: Yiming Qian <yimingqian591@gmail.com>
Fixes: 2c6e68fbc523 ("crypto: pcrypt - call the complete function on error")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

crypto/pcrypt.c
@@ -69,6 +69,9 @@ static void pcrypt_aead_done(void *data, int err)
 	struct pcrypt_request *preq = aead_request_ctx(req);
 	struct padata_priv *padata = pcrypt_request_padata(preq);
 
+	if (err == -EINPROGRESS)
+		return;
+
 	padata->info = err;
 
 	padata_do_serial(padata);
@@ -82,7 +85,7 @@ static void pcrypt_aead_enc(struct padata_priv *padata)
 
 	ret = crypto_aead_encrypt(req);
 
-	if (ret == -EINPROGRESS)
+	if (ret == -EINPROGRESS || ret == -EBUSY)
 		return;
 
 	padata->info = ret;
@@ -133,7 +136,7 @@ static void pcrypt_aead_dec(struct padata_priv *padata)
 
 	ret = crypto_aead_decrypt(req);
 
-	if (ret == -EINPROGRESS)
+	if (ret == -EINPROGRESS || ret == -EBUSY)
 		return;
 
 	padata->info = ret;